80844daee21341dfbc2bc16f60f1f8fdddf41f28
Agustin Benoit <benoit.agustin@gmail.com>
2022-04-05 16:25:08 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 echo "Pulling changes on ${LINODEVM}"
 
-sshpass -p 'admin123' ssh -oStrictHostKeyChecking=no ghaction@agusbenoit.com /home/ghaction/pull.sh
-#echo "sshpass -p ${LINODEUSERPWD} ssh -oStrictHostKeyChecking=no ${LINODEUSER}@${LINODEVM} /home/ghaction/pull.sh"
+sshpass -p ${LINODEUSERPWD} ssh -oStrictHostKeyChecking=no ${LINODEUSER}@${LINODEVM} /home/ghaction/pull.sh
 
 echo ${?}
