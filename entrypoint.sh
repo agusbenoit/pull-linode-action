ad1cd6d80cd817d01836f34a0457315aee813149
Agustin Benoit <abenoit@frba.utn.edu.ar>
2022-04-05 16:06:58 -0300

    Action first try

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -l
+
+echo "Pulling changes on ${LINODEVM}"
+
+#sshpass -p 'admin123' ssh -oStrictHostKeyChecking=no ghaction@agusbenoit.com /home/ghaction/pull.sh
+echo "sshpass -p ${LINODEUSERPWD} ssh -oStrictHostKeyChecking=no ${LINODEUSER}@${LINODEVM} /home/ghaction/pull.sh"
+
+echo ${?}
